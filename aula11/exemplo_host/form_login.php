e4639a0ecc6e22ec97c122dc30e652b4b35ee5c5
pexin69s <kauan.dias@alunos.ifsuldeminas.edu.br>
2023-03-28 08:42:03 -0300

    aula11

---
+++ aula11/exemplo_host/form_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Entra aí </h1>
+    <hr>
+
+        <form action="login.php" method="post">
+
+            <label for="user"> Usuário </label> <br>
+            <input type="text" name="user" id="user"> <br>
+
+            <label for="pw"> Senha </label> <br>
+            <input type="password" name="pw" id="pw"> <br>
+
+            <button type="submit"> Entrar </button>
+        </form>
+</body>
+</html>
---
+++ aula11/exemplo_host/login.php
@@ -0,0 +1,17 @@
+<h1> Pexin </h1>
+
+<?php
+
+      if(isset($_POST["user"])){
+   if($_POST["user"] == "admin" && $_POST["pw"] == 123) {
+    echo "Usuário logado";
+   }
+   else{
+    echo "Usuário ou senha inválidos!";
+   }
+}
+   else{
+      header("Location: form_login.php");
+   }
+
+?>
