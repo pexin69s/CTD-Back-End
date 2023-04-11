82cd9a732366c7e8c497a383b60684f3af99e319
pexin69s <kauan.dias@alunos.ifsuldeminas.edu.br>
2023-04-11 07:14:16 -0300

    aula12

---
+++ aula12/rodape.php
@@ -0,0 +1,13 @@
+<hr>
+
+
+    <!-- Rodape -->
+    <footer class="bg-dark p-3 text-white text-center">
+          Meu site de noticias @ 2023
+    </footer>
+    <!-- Final do rodapé -->  
+    
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  </body>
+</html>
