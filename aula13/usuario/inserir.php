cac7777262ea91ab4d4f0a0bbc4817c191dab147
pexin69s <kauan.dias@alunos.ifsuldeminas.edu.br>
2023-04-26 09:53:30 -0300

    aula13

--- aula13/usuario/inserir.php
+++ aula13/usuario/inserir.php
@@ -1,16 +1,14 @@
 <?php
 
-    //importa o arquivo de conexão
-    require_once "../conexao.php";
+//importa o arquivo de conexão
+require_once "../conexao.php";
 
-    //cria uma variável com um comando SQL
-    $SQL = "INSERT INTO `usuario` (`nome`, `login`, `senha`) VALUES ('kauan', 'admin123', '123');";
+//cria uma variável com um comando SQL
+$SQL = "INSERT INTO `usuario` (`nome`, `login`, `senha`) VALUES ('Kauan Pereira', 'admin123', '123');";
 
-    //prepara o comando para ser execultado no mysql
-    $comando = $conexao->prepare($SQL);
+//prepara o comando para ser executado no mysql
+$comando = $conexao->prepare($SQL);
 
-    //executa o comando 
-    $comando->execute();
-
-
+//executa o comando 
+$comando->execute();
 
