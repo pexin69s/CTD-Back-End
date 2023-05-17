3363d56e8e3035b149613a3fa0699046b78bfaab
pexin69s <125364806+pexin69s@users.noreply.github.com>
2023-05-17 09:36:15 -0300

    Add files via upload

---
+++ aula13/usuario/inserir.php
@@ -0,0 +1,29 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../conexao.php";
+
+   //verifica se o nome, login e senha foram enviado
+   //do formulário
+   if(isset($_POST['nome']) && isset($_POST['login']) &&
+     isset($_POST['senha'])){
+
+   $nome = $_POST['nome'];
+   $login = $_POST['login'];
+   $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
+
+   //cria uma variável com um comando SQL
+   $SQL = "INSERT INTO `usuario` (`nome`, `login`, `senha`) VALUES (?, ?, ?);";
+ 
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   //faz a vinculação dos parâmetros ?, ?, ?
+   $comando->bind_param("sss", $nome, $login, $senha);
+
+   //executa o comando
+   $comando->execute();
+
+}
+   //volta para o formulário
+   header("Location: ../usuarios.php");
