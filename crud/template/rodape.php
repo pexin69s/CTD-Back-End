2a73efd1e050db662c09dd12514ff6f1fa415a47
pexin69s <kauan.dias@alunos.ifsuldeminas.edu.br>
2023-06-15 14:44:54 -0300

    Nicoly

--- crud/template/rodape.php
+++ crud/template/rodape.php
@@ -1,7 +1,7 @@
 <hr>
 <!-- Rodape-->
 <footer class="bg-dark p-3 text-white text=center">
-  Meu site de atores @ 2023 Mano Pexin
+  Meu site de atores @ 2023 Mano Pexin(Kauan Pereira Dias)
     </footer>
 <!--Final do rodape-->
 
