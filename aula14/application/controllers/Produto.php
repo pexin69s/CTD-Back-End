0fd99e0c4674f2e819c88d2ae826b3803ebd7e76
pexin69s <kauan.dias@alunos.ifsuldeminas.edu.br>
2023-06-27 07:33:17 -0300

    aula14

---
+++ aula14/application/controllers/Produto.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Produto extends CI_Controller {
+
+	public function index()
+	{
+		$crud = new Grocery_CRUD();
+	$crud->set_table("noticia");
+	$crud->set_field_upload("foto");
+	$form = $crud->render();
+	$this->load->view('crud', $form);
+}
+	}
+
+
+
+
+
+
